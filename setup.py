7bf5b23c8fc890ba05445ec004879813e460a8b1
Benjamin De Worsop <ben@swoop.aero>
2023-04-20 12:11:31 +1000

    try build with our setup style

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-#!/usr/bin/env python3
-
-import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup()
+from setuptools import setup, find_packages
+import version
+setup(name='mavproxymanager',
+      version=version.get_git_version(),
+      description='Pyubx2',
+      url='https://github.com/deworben/pyubx2.git',
+      author='Benjamin De Worsop',
+      author_email='ben@swoop.aero',
+      license='MIT',
+      packages=find_packages(),
+      package_data={},
+      # Actually requires companionCode install_requires=["flight-controller-sim==4.1.80", "MAVProxy==3.9.0", "aircraft-communication==1.0.24"],
+      include_package_data=True,  # include files specified in MANIFEST.in
+      zip_safe=False)
